b0e869c09286e8c809d737911b7044f4567e7f0a
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2022-10-18 00:03:36 +0300

    feat: made solutions for some tasks in task basic-js

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let result = 0;
+  for (let i=0; i<matrix.length; i++) {
+       for (let k=0; k < matrix[i].length; k++) {
+        let item = matrix[i][k];
+       if (item == '^^') {
+        result = result + 1;
+       } 
+       }
+    
+  }
+  return result;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let filtredMembers = members.filter((item) => {
+    return typeof item === 'string';
+  })
+  let firstLetters = filtredMembers.map((item) => {
+    
+    return item.toUpperCase().trim()[0];
+
+  })
+  return firstLetters.sort().join('');
+  
 }
 
 module.exports = {
